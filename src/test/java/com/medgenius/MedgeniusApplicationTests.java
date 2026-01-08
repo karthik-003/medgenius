0598cd4266d0d8480de7dc6f3128e8904df71e46
Karthik <karthikdy@gmail.com>
2026-01-08 22:33:21 +0530

    Adding misc files

---
+++ src/test/java/com/medgenius/MedgeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medgenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedgeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
